f07c53ce0cffc6aa6b50dd6d783dcd0e32538bd5
Siftab <siiffuuu@gmail.com>
2024-09-26 19:41:02 +0600

    implemented local redux state

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import "./App.css";
+import { decrement, increment } from "./redux/features/counterSlice";
+import { useAppDispatch, useAppSelector } from "./redux/Hook";
+import { RootState } from "./redux/store";
 
 function App() {
+
+const {count} = useAppSelector((state:RootState)=>state.counter)
+const dispatch = useAppDispatch()
   return (
     <div className="h-screen flex justify-center items-center flex-col space-y-10">
       <h1 className="text-3xl">react+ redux+typescript</h1>
       <div className=" flex space-x-10 items-center p-10 border border-purple-700 rounded-md">
-        <button className="px-4 py-3 text-white bg-green-500 rounded-md">increment</button>
-        <p> count number</p>
-        <button className="px-4 py-3 text-white bg-red-500 rounded-md">decrement</button>
+        <button className="px-4 py-3 text-white bg-green-500 rounded-md" onClick={()=>dispatch(increment())}>increment</button>
+        <p>{count} </p>
+        <button className="px-4 py-3 text-white bg-red-500 rounded-md" onClick={()=>dispatch(decrement())}>decrement</button>
       </div>
     </div>
   );
